f988e16f3f8f2b2c3d2576f775a2f3393a700cf1
Austin W. Milne <awbmilne@gmail.com>
2022-04-03 12:48:50 -0400

    Clean up working commit

--- Q1_1D_Heat_Eqn_Spherical.m
+++ Q1_1D_Heat_Eqn_Spherical.m
@@ -22,7 +22,7 @@ k = .500; % conductivity of egg [W K^-1 m^-1]
 rho = 1035; % Density of egg [kg m^-3]
 c_p = 3200; % Specific heat of Egg [J kg^-1 K^-1]
 N = 100; % number of grid points
-dt=0.001; % Size of time step
+dt=0.01; % Size of time step
 dr=R/N;  % grid spacing
 
 alpha = k / (rho*c_p); % Increment coefficient
@@ -56,16 +56,14 @@ while any(T(:,k) < T_min); % While the minimum temp isnt reached
         T(i,k+1)=T(i,k) + alpha*dt*(d2T_dr2 + (2/r)*dT_dr); % Increment T
     end
     time = k * dt;
-    % fprintf("Time: %5.6f - Temp: %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(end-1,k), T(end,k))
     if not(mod(time, 1))
         fprintf("Time: %5.6f - Temp: %3.14f, %3.14f, %3.14f, %3.14f, %3.14f, %3.14f, %3.14f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(N-1,k))
     end
     k = k + 1;
 end
 
-size = length(T(1,:))
+size = length(T(1,:));
 time = size * dt
-T(:,end)
 
 % Visuaization ------------------------------------------------------------
 % line plot
@@ -78,8 +76,5 @@ plot(T(:,indexes(2)))
 plot(T(:,indexes(3)))
 plot(T(:,indexes(4)))
 plot(T(:,indexes(5)))
-% plot(T(:,1000))
-% plot(T(:,10000))
-% plot(T(:,20000))
 xlabel('x'); ylabel('T(x,t)');
 legend(split(sprintf('t = %i sec,', round(times(:))), ","));
