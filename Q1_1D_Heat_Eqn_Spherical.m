b79e6c3839673acfc77358f91947b8a3bc8e8671
Austin W. Milne <awbmilne@gmail.com>
2022-04-03 00:04:35 -0400

    FUCKING WORKING

--- Q1_1D_Heat_Eqn_Spherical.m
+++ Q1_1D_Heat_Eqn_Spherical.m
@@ -14,20 +14,20 @@
 clear all; clc; close all;
 
 % Parameter definitions ---------------------------------------------------
-R = 0.02625; % Radius of egg [M]
-T_start = 20; % Room temperature in celsius
+R = .02625; % Radius of egg [M]
+T_start = 5; % Room temperature in celsius
 T_min = 80; % Minimum cooking temp throughout
 T_max = 100;
-k = 0.5; % conductivity of egg [W K^-1 M^-1]
-rho = 1035; % Density of egg [Kg M^-3]
-c_p = 3.2*10e-3; % Specific heat of Egg [J Kg^-1 K^-1]
+k = .500; % conductivity of egg [W K^-1 m^-1]
+rho = 1035; % Density of egg [kg m^-3]
+c_p = 3200; % Specific heat of Egg [J kg^-1 K^-1]
 N = 100; % number of grid points
 dt=0.001; % Size of time step
 dr=R/N;  % grid spacing
-% alpha = k / (rho*c_p) * dt * dr^2 / (2 * dr); % Increment coefficient
-alpha = k / (rho*c_p) * dt * dr / (2 * dr); % Increment coefficient
-% alpha = k * dt * dr^2 / (2 * dr); % Increment coefficient
-% alpha = k * dt * dr / (2 * dr); % Increment coefficient
+
+alpha = k / (rho*c_p); % Increment coefficient
+coeff = 1-2*alpha*dt/(dr^2);
+fprintf("alpha = %f, coeff = %f \n", alpha, coeff);
 
 % Initialization ----------------------------------------------------------
 % solution grid
@@ -46,35 +46,40 @@ T(end,1) = T_max;
 % Time: explicit time marching, i.e, k+1 values are determined by k values
 % BCs: Dirichlet (constant temperature)
 k = 1;
-while any(T(:,k) < T_min) % While the minimum temp isnt reached
-    T(1,k+1) = T(2,k); % Set center to same as 1 dr away from center
+while any(T(:,k) < T_min); % While the minimum temp isnt reached
+    T(1,k+1) = max(T(2,k) - (T(3,k) - T(2,k)), T_start); % Set center to 1 'slope' away from i=2, Dont go down tho
     T(end,k+1) = T(end,k); % Keep end value
     for i=2:N % space
-        T(i,k+1)=T(i,k) + alpha*(T(i+1,k)-T(i-1,k)); % Increment T
-        T(i,k+1) = (T(i,k+1) > T_max) * T_max + not(T(i,k+1) > T_max) * T(i,k+1); % Branchless limit T to T_max
+        r = (i-1) * dr;
+        d2T_dr2 = (T(i+1,k)-2*T(i,k)+T(i-1,k))/(dr^2);
+        dT_dr = (T(i+1,k)-T(i-1,k))/(2*dr);
+        T(i,k+1)=T(i,k) + alpha*dt*(d2T_dr2 + (2/r)*dT_dr); % Increment T
     end
     time = k * dt;
-    % fprintf("Time: %5.1f - Temp: %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(N-1,k))
+    % fprintf("Time: %5.6f - Temp: %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(end-1,k), T(end,k))
     if not(mod(time, 1))
-        fprintf("Time: %5.1f - Temp: %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f, %3.10f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(N-1,k))
+        fprintf("Time: %5.6f - Temp: %3.14f, %3.14f, %3.14f, %3.14f, %3.14f, %3.14f, %3.14f \n", time, T(1, k), T(N/100, k), T(N/10, k), T(N*5/10,k), T(N*9/10,k), T(N*99/100,k), T(N-1,k))
     end
     k = k + 1;
 end
 
 size = length(T(1,:))
 time = size * dt
+T(:,end)
 
 % Visuaization ------------------------------------------------------------
 % line plot
+indexes = [1, round(size/4), round(size/2), round(size*3/4), round(size)];
+times = indexes * dt;
 figure(1)
-plot(T(:,1)); hold on 
-plot(T(:,round(size/4)))
-plot(T(:,round(size/2)))
-plot(T(:,round(size*3/4)))
-plot(T(:,round(size)))
+hold on;
+plot(T(:,indexes(1)))
+plot(T(:,indexes(2)))
+plot(T(:,indexes(3)))
+plot(T(:,indexes(4)))
+plot(T(:,indexes(5)))
 % plot(T(:,1000))
 % plot(T(:,10000))
 % plot(T(:,20000))
 xlabel('x'); ylabel('T(x,t)');
-legend('t=0','t=0.0005','t=0.005','t=0.05','t=0.5','t=1')
-
+legend(split(sprintf('t = %i sec,', round(times(:))), ","));
